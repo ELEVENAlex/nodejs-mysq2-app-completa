e18a33b42c1f1cd06c175b406e6feb902b10e7ed
Alex Manet <alexander.manap@gmail.com>
2023-02-17 10:06:35 +0100

    Diseño de navbar para logout

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport')
+const { isLoggedIn, isNotLoggedIn } = require('../lib/auth')
 
 /* GET users listing. */
-router.get('/signup', (req, res, next) => {
+router.get('/signup', isNotLoggedIn, (req, res, next) => {
   res.render('auth/signup');
 });
 
-router.post('/signup', passport.authenticate('local.signup', {
+router.post('/signup', isNotLoggedIn, passport.authenticate('local.signup', {
   successRedirect: '/profile',
   failureRedirect: '/signup',
   failureFlash: true
 }))
 
 
-router.get('/signin', (req, res) => {
+router.get('/signin', isNotLoggedIn, (req, res) => {
   res.render('auth/signin')
 })
 
-router.post('/signin', (req, res, next) => {
+router.post('/signin', isNotLoggedIn, (req, res, next) => {
   passport.authenticate('local.signin', {
     successRedirect: '/profile',
     failureRedirect: '/signin',
@@ -27,11 +28,11 @@ router.post('/signin', (req, res, next) => {
 })
 
 
-router.get('/profile', (req, res) => {
+router.get('/profile', isLoggedIn, (req, res) => {
   res.render('profile')
 })
 
-router.get('/logout', (req, res) => {
+router.get('/logout', isLoggedIn, (req, res) => {
 
   req.logOut(function(err){
     if(err) return next(err)
